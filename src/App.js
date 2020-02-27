38bc4745991ba26c0af955553df949f5e2601846
Claudiferock <claudio.rodriguez.vasquez@myy.haaga-helia.fi>
2020-02-27 11:40:58 +0200

    Add trophylicious Repo

--- src/App.js
+++ src/App.js
@@ -5,20 +5,33 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav>
+          <a>bla</a>
+          <a>bla</a>
+          <a>bla</a>
+        </nav>
       </header>
+
+      <div className="App-content">
+        <div className="Feature-items">
+          <div className="feature__main"> Main</div>
+          <div className="feature__main--img"> main img</div>
+          <div className="feature__main--details"> main details</div>
+          <div className="feature__main--highlight"> main highlight</div>
+          <div className="feature__secondary__item1"> secondary item1</div>
+          <div className="feature__secondary__item2"> secondary item2</div>
+          <div className="feature__secondary__item3"> secondary item3</div>
+          <div className="feature__secondary--highlight"> secondary highlight</div>
+          <div className="feature__secondary--more"> more</div>
+        </div> 
+
+      </div>
+
+      <footer className="App-footer">
+        <p>footer</p>
+      </footer>
     </div>
   );
 }
