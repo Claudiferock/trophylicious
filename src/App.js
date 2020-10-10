7f9aa8f977ebe91baba078bfc7388b09ca129c59
Claudio <claudifero@gmail.com>
2020-10-10 14:44:19 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ducksAward from './assets/img/ducks.jpg';
 import beerPongAward from './assets/img/beer_pong.JPG';
-import okAward from './assets/img/ok.jpg';
+import favio from './assets/img/favio.gif';
 import tacoAward from './assets/img/taco.jpg';
+import spoonAward from './assets/img/spoon.jpg';
 import './App.css';
 
 function App() {
@@ -10,43 +11,45 @@ function App() {
     <div className="App">
 
       <header className="App-header">
-        <nav>
-          <a>Categories</a>
-          <a>About</a>
-          <a>Contact</a>
-          <a>Log In</a>
-          <a>Basquet</a>
+        <nav className="nav__list">
+          <li><a>Categories</a></li>
+          <li><a>About</a></li>
+          <li><a>Contact</a></li>
+          <li><a>Log In</a></li>
+          <li><a>Basquet</a></li>
         </nav>
       </header>
 
       <div className="App-content">
-        <div className="Feature-items">
+
+        <div className="Welcome">
+          <img src={favio} alt="Favio welcomes you to Trophylicious" width="720px"/>
+          <div className="welcome-info">
+            <p>We all deserve recognition for our... "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."</p>
+            <a href="#feature-items" class="btn btn--white btn--animated">Go to feature trophies</a>
+          </div>
+        </div>
+        <div className="Feature-items" id="feature-items">
           <div className="feature__main"> Main</div>
           <div className="feature__main--img">
-            <img src={ducksAward} alt="Quack awards" />
+            <img src={ducksAward} alt="Quack awards" loading="lazy"/>
           </div>
           <div className="feature__main--details"> main details</div>
           <div className="feature__main--highlight" alt="main highlight">60% OFF</div>
 
-          <div className="feature__secondary__item1">
-            <h2>Beer Pong</h2>
-            <img src={beerPongAward} alt="Beer Pong award" />
+          <div className="feature__secondary__item1">            
+            <img src={beerPongAward} alt="Beer Pong award" loading="lazy"/>
             <div className="priceTag">
-
             </div>
           </div>
-          <div className="feature__secondary__item2"> 
-            <h2>Perfect</h2>
-            <img src={okAward} alt="Ok award" />
-            <div className="priceTag">
-              
+          <div className="feature__secondary__item2">            
+            <img src={spoonAward} alt="spoon award" loading="lazy"/>
+            <div className="priceTag">              
             </div>
           </div>
-          <div className="feature__secondary__item3"> 
-            <h2>Taco</h2>
-            <img src={tacoAward} alt="Taco award" />
-            <div className="priceTag">
-              
+          <div className="feature__secondary__item3">             
+            <img src={tacoAward} alt="Taco award" loading="lazy"/>
+            <div className="priceTag">              
             </div>
           </div>
           <div className="feature__secondary--highlight" alt="secondary highlight">24% OFF</div>
