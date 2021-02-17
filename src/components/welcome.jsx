862c9028c801495f8f6bfae00039effffba24d05
Claudio <claudifero@gmail.com>
2021-02-17 12:03:25 +0200

    Add UI fixes

--- src/components/welcome.jsx
+++ src/components/welcome.jsx
@@ -1,16 +1,15 @@
 import React from "react";
-import favio from '../assets/img/favio.gif';
+//import favio from '../assets/img/favio.gif';
 import '../App.css';
 
 const Welcome = () => (
     <>
-        <div className="Welcome" id="welcome">
-          <img src={favio} alt="Favio welcomes you to Trophylicious" width="720px" loading="lazy"/>
-          <div className="welcome-info">
+        <header className="Welcome" id="welcome">
+          <section className="welcome-info">
             <p>We all deserve to be recognized and valued. Show it with a trophy! there's a trophy for anything<br/><br/><q className="welcome-quote">The trophy is earned in the hour that no one is watching</q></p>
             <a href="#feature-items" className="btn btn--white btn--animated">Feature trophies</a>
-          </div>
-        </div>
+          </section>
+        </header>
     </>
 );
 
